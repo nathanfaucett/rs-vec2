1f4c22706ec3beb892c6969921604c4d1f7111bc
nathanfaucett <nathanfaucett@gmail.com>
2015-07-05 08:42:04 -0500

    updates

--- src/create.rs
+++ src/create.rs
@@ -11,17 +11,6 @@ fn test_create() {
 }
 
 #[inline(always)]
-pub fn zero<T: Num>() -> [T; 2] {create(T::zero(), T::zero())}
-#[inline(always)]
-pub fn up<T: Num>() -> [T; 2] {create(T::zero(), T::one())}
-#[inline(always)]
-pub fn down<T: Num>() -> [T; 2] {create(T::zero(), -T::one())}
-#[inline(always)]
-pub fn right<T: Num>() -> [T; 2] {create(T::one(), T::zero())}
-#[inline(always)]
-pub fn left<T: Num>() -> [T; 2] {create(-T::one(), T::zero())}
-
-#[inline(always)]
 pub fn clone<T: Num>(v: [T; 2]) -> [T; 2] {create(v[0], v[1])}
 
 #[inline(always)]
--- src/set.rs
+++ src/set.rs
@@ -13,3 +13,16 @@ fn test_set() {
     set(&mut v, 1, 2);
     assert!(v == [1, 2]);
 }
+
+#[inline(always)]
+pub fn zero<T: Num>(out: &mut [T; 2]) -> &mut [T; 2] { set(out, T::zero(), T::zero()) }
+#[inline(always)]
+pub fn identity<T: Num>(out: &mut [T; 2]) -> &mut [T; 2] { set(out, T::zero(), T::zero()) }
+#[inline(always)]
+pub fn up<T: Num>(out: &mut [T; 2]) -> &mut [T; 2] { set(out, T::zero(), T::one()) }
+#[inline(always)]
+pub fn down<T: Num>(out: &mut [T; 2]) -> &mut [T; 2] { set(out, T::zero(), -T::one()) }
+#[inline(always)]
+pub fn left<T: Num>(out: &mut [T; 2]) -> &mut [T; 2] { set(out, -T::one(), T::zero()) }
+#[inline(always)]
+pub fn right<T: Num>(out: &mut [T; 2]) -> &mut [T; 2] { set(out, T::one(), T::zero()) }
